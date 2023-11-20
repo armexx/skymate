18b9cdbb6017f36db21f9fceb42aae1f6270650d
aniket <arianprime1234@gmail.com>
2023-11-20 22:52:34 +0530

    publishing the app files

---
+++ js/cities.js
@@ -0,0 +1,326 @@
+export let cities = {
+    "Netherlands": [
+      {
+        "name": "Amsterdam",
+        "lat": 52.3676,
+        "lon": 4.9041
+      },
+      {
+        "name": "Rotterdam",
+        "lat": 51.9225,
+        "lon": 4.47917
+      }
+    ],
+    "Turkey": [
+      {
+        "name": "Istanbul",
+        "lat": 41.0082,
+        "lon": 28.9784
+      },
+      {
+        "name": "Ankara",
+        "lat": 39.9334,
+        "lon": 32.8597
+      }
+    ],
+    "Sweden": [
+      {
+        "name": "Stockholm",
+        "lat": 59.3293,
+        "lon": 18.0686
+      },
+      {
+        "name": "Gothenburg",
+        "lat": 57.7089,
+        "lon": 11.9746
+      }
+    ],
+    "Greece": [
+      {
+        "name": "Athens",
+        "lat": 37.9838,
+        "lon": 23.7275
+      },
+      {
+        "name": "Thessaloniki",
+        "lat": 40.6401,
+        "lon": 22.9444
+      }
+    ],
+    "UK": [
+      {
+        "name": "London",
+        "lat": 51.5074,
+        "lon": -0.1278
+      },
+      {
+        "name": "Manchester",
+        "lat": 53.4830,
+        "lon": -2.2426
+      }
+    ],
+    "Spain": [
+      {
+        "name": "Madrid",
+        "lat": 40.4168,
+        "lon": -3.7038
+      },
+      {
+        "name": "Barcelona",
+        "lat": 41.3851,
+        "lon": 2.1734
+      }
+    ],
+    "Germany": [
+      {
+        "name": "Berlin",
+        "lat": 52.5200,
+        "lon": 13.4050
+      },
+      {
+        "name": "Munich",
+        "lat": 48.1351,
+        "lon": 11.5820
+      }
+    ],
+    "Switzerland": [
+      {
+        "name": "Zurich",
+        "lat": 47.3769,
+        "lon": 8.5417
+      },
+      {
+        "name": "Geneva",
+        "lat": 46.2044,
+        "lon": 6.1432
+      }
+    ],
+    "Belgium": [
+      {
+        "name": "Brussels",
+        "lat": 50.8503,
+        "lon": 4.3517
+      },
+      {
+        "name": "Antwerp",
+        "lat": 51.2199,
+        "lon": 4.3963
+      }
+    ],
+    "Romania": [
+      {
+        "name": "Bucharest",
+        "lat": 44.4268,
+        "lon": 26.1025
+      },
+      {
+        "name": "Cluj-Napoca",
+        "lat": 46.7712,
+        "lon": 23.6236
+      }
+    ],
+    "Hungary": [
+      {
+        "name": "Budapest",
+        "lat": 47.4979,
+        "lon": 19.0402
+      },
+      {
+        "name": "Debrecen",
+        "lat": 47.5316,
+        "lon": 21.6273
+      }
+    ],
+    "Denmark": [
+      {
+        "name": "Copenhagen",
+        "lat": 55.6761,
+        "lon": 12.5683
+      },
+      {
+        "name": "Aarhus",
+        "lat": 56.1629,
+        "lon": 10.2039
+      }
+    ],
+    "Ireland": [
+      {
+        "name": "Dublin",
+        "lat": 53.3498,
+        "lon": -6.2603
+      },
+      {
+        "name": "Cork",
+        "lat": 51.8969,
+        "lon": -8.4863
+      }
+    ],
+    "Italy": [
+      {
+        "name": "Rome",
+        "lat": 41.9028,
+        "lon": 12.4964
+      },
+      {
+        "name": "Milan",
+        "lat": 45.4642,
+        "lon": 9.1900
+      }
+    ],
+    "France": [
+      {
+        "name": "Paris",
+        "lat": 48.8566,
+        "lon": 2.3522
+      },
+      {
+        "name": "Marseille",
+        "lat": 43.2965,
+        "lon": 5.3698
+      }
+    ],
+    "Portugal": [
+      {
+        "name": "Lisbon",
+        "lat": 38.7223,
+        "lon": -9.1393
+      },
+      {
+        "name": "Porto",
+        "lat": 41.1579,
+        "lon": -8.6291
+      }
+    ],
+    "British Territory": [
+      {
+        "name": "Gibraltar",
+        "lat": 36.1377,
+        "lon": -5.3453
+      },
+      {
+        "name": "Bermuda",
+        "lat": 32.3078,
+        "lon": -64.7505
+      }
+    ],
+    "Finland": [
+      {
+        "name": "Helsinki",
+        "lat": 60.1695,
+        "lon": 24.9354
+      },
+      {
+        "name": "Tampere",
+        "lat": 61.4978,
+        "lon": 23.7610
+      }
+    ],
+    "Ukraine": [
+      {
+        "name": "Kyiv",
+        "lat": 50.4501,
+        "lon": 30.5234
+      },
+      {
+        "name": "Kharkiv",
+        "lat": 49.9935,
+        "lon": 36.2304
+      }
+    ],
+    "Norway": [
+      {
+        "name": "Oslo",
+        "lat": 59.9139,
+        "lon": 10.7522
+      },
+      {
+        "name": "Bergen",
+        "lat": 60.3913,
+        "lon": 5.3221
+      }
+    ],
+    "Czech Republic": [
+      {
+        "name": "Prague",
+        "lat": 50.0755,
+        "lon": 14.4378
+      },
+      {
+        "name": "Brno",
+        "lat": 49.1951,
+        "lon": 16.6068
+      }
+    ],
+    "Iceland": [
+      {
+        "name": "Reykjavik",
+        "lat": 64.1353,
+        "lon": -21.8952
+      },
+      {
+        "name": "Akureyri",
+        "lat": 65.6840,
+        "lon": -18.1000
+      }
+    ],
+    "Latvia": [
+      {
+        "name": "Riga",
+        "lat": 56.9496,
+        "lon": 24.1052
+      },
+      {
+        "name": "Daugavpils",
+        "lat": 55.8835,
+        "lon": 26.5330
+      }
+    ],
+    "Bulgaria": [
+      {
+        "name": "Sofia",
+        "lat": 42.6977,
+        "lon": 23.3219
+      },
+      {
+        "name": "Plovdiv",
+        "lat": 42.1354,
+        "lon": 24.7453
+      }
+    ],
+    "Estonia": [
+      {
+        "name": "Tallinn",
+        "lat": 59.4370,
+        "lon": 24.7536
+      },
+      {
+        "name": "Tartu",
+        "lat": 58.3776,
+        "lon": 26.7290
+      }
+    ],
+    "Austria": [
+      {
+        "name": "Vienna",
+        "lat": 48.2082,
+        "lon": 16.3738
+      },
+      {
+        "name": "Graz",
+        "lat": 47.0707,
+        "lon": 15.4395
+      }
+    ],
+    "Poland": [
+      {
+        "name": "Warsaw",
+        "lat": 52.229,
+        "lon": 21.012
+      },
+      {
+        "name": "Krakow",
+        "lat": 50.065,
+        "lon": 19.945
+      }
+    ]
+  }
---
+++ js/main.js
@@ -0,0 +1,282 @@
+//Importing the cities variable from cities.js file
+import { cities } from '/js/cities.js';
+let dt = new Date();
+let options = {
+    weekday: "long",
+    year: "numeric",
+    month: "short",
+    day: "numeric"
+};
+let today = dt.toLocaleDateString("en-US", options);
+let hours = dt.getHours();
+//Function to change background image as per day cycle
+function bgChange(){
+    let bgchange = document.getElementById("bg-change");
+    let bgDawn = "bg-dawn";
+    let bgDay = "bg-day";
+    let bgDusk = "bg-dusk"
+    let bgNight = "bg-night";
+    if(hours >= 17 && hours < 18){
+        bgchange.classList.remove(bgDay);
+        bgchange.classList.add(bgDusk);
+    }else if(hours >= 18 || hours < 5){
+        bgchange.classList.remove(bgDusk);
+        bgchange.classList.add(bgNight);
+    }else if(hours >= 5 && hours < 6){
+        bgchange.classList.remove(bgNight);
+        bgchange.classList.add(bgDawn);
+    }else{
+        bgchange.classList.remove(bgDawn);
+        bgchange.classList.add(bgDay);
+    }
+}
+bgChange();
+const selectCity = document.getElementById("select-city");
+//Function to retrieve city names and display them as options
+function getCities(){
+    let ct = 0;
+    let citiesKey = [];
+    for (let key in cities){
+        let citiesData = cities[key]; // citiesData stores each country name along with their cities
+        citiesKey[ct] = citiesData; //citiesKey is an array to store only the cities
+        for (let i = 0; i < 2; i++){ //Nested Loops through the first two cities of each country.
+            let option = document.createElement('option');
+            option.value = citiesData[i].name;
+            option.text = citiesData[i].name + ", " + key;
+            selectCity.appendChild(option);
+        }
+        ct++;
+    }
+    return citiesKey;
+}
+
+const fetchStat = document.getElementById("fetchStat");
+//Function that fetches weather forecast data from the 7timer API based on lat long coordinates and updates the forecast display
+let contain = document.getElementById("contain");
+let cityName = document.getElementById("cityName");
+let date = document.querySelectorAll(".w-date");
+let wstat = document.querySelectorAll(".w-stat");
+let tempH = document.querySelectorAll(".wtemp-h");
+let tempL = document.querySelectorAll(".wtemp-l");
+let img = document.querySelectorAll(".wimg");
+let longitude = 88.3639;
+let latitude = 22.5726;
+
+function getWeatherData(longitude,latitude){
+    fetch(`https://www.7timer.info/bin/api.pl?lon=${longitude}&lat=${latitude}&product=civillight&output=json`)
+    .then(response => response.json())
+    .then(data =>{
+        fetchStat.style.display = "none";
+        contain.style.display = "block";
+        //Looping through data.dataseries array of objects from api
+        for (let i = 0; i < data.dataseries.length; i++){
+            let ds = data.dataseries[i];
+            wstat[i].textContent = ds.weather;
+            tempH[i].textContent = ds.temp2m.max + "°C";
+            tempL[i].textContent = ds.temp2m.min + "°C";
+            //Storing the returned value from the readableDate function in the dateValue variable.
+            let dateValue = readableDate(ds.date); //dateValue is a string. api send date as 20231115
+            let dateObject = new Date(dateValue); // output example : Tue May 10 2022 00:00:00 GMT+0000 (Coordinated Universal Time)
+            let d = dateObject.toLocaleDateString('en-US', {
+                weekday: 'short',
+                month: 'short',
+                day: 'numeric'
+            });
+            date[i].textContent = d;
+            // (ds.weather === "clear" && hours >= 18 || hours < 5)
+            if (ds.weather === "clear"){
+                img[i].src = "images/clear.svg";
+            }
+            if (ds.weather === "cloudy"){
+                img[i].src = "images/cloud.svg";
+            }
+            if (ds.weather === "fog"){
+                img[i].src = "images/fog.svg";
+            }
+            if (ds.weather === "ishower") {
+                img[i].src = "images/ishower.svg";
+            }
+            if (ds.weather === "oshower") {
+                img[i].src = "images/ishower.svg";
+            }
+            if (ds.weather === "lightrain") {
+                img[i].src = "images/lightrain.svg";
+            }
+            if (ds.weather === "lightsnow") {
+                img[i].src = "images/lightsnow.svg";
+            }
+            if (ds.weather === "snow") {
+                img[i].src = "images/snow.svg";
+            }
+            if (ds.weather === "mcloudy"){
+                img[i].src = "images/cloudy.svg";
+            }
+            if (ds.weather === "pcloudy") {
+                img[i].src = "images/cloudy.svg";
+            }
+            if (ds.weather === "rain") {
+                img[i].src = "images/rain.svg";
+            }
+            if (ds.weather === "rainsnow") {
+                img[i].src = "images/rainsnow.svg";
+            }
+            if (ds.weather === "ts") {
+                img[i].src = "images/tsrain.svg";
+            }
+            if (ds.weather === "tstorm") {
+                img[i].src = "images/tstorm.svg";
+            }
+        }
+    })
+    .catch(error => {
+        console.error('Fetch error:', error);
+    });
+}
+
+getWeatherData(longitude,latitude);
+const citiesData = getCities();
+
+// Function to fetch weather data and dynamically update background
+let wDate = document.getElementById("wDate");
+let tempMx = document.getElementById("wtempH");
+let tempMn = document.getElementById("wtempL");
+let wImg = document.getElementById("wImg");
+let wStat = document.getElementById("wStat");
+async function fetchWeather(longitude, latitude){
+    try{
+      const response = await fetch(`https://www.7timer.info/bin/api.pl?lon=${longitude}&lat=${latitude}&product=civillight&output=json`);
+      const weatherData = await response.json();
+      fetchStat.style.display = "none";
+      contain.style.display = "block";
+      // Extract required information
+      const tempMax = weatherData.dataseries[0].temp2m.max; // Temperature at 2 meters
+      const tempMin = weatherData.dataseries[0].temp2m.min;
+      const weatherStatus = weatherData.dataseries[0].weather;
+
+      if(weatherStatus === 'clear'){
+        wDate.textContent = today;
+        wStat.textContent = "clear";
+        wImg.src = "images/clear.svg";
+        bgWeather.src = "images/clear.jpg";
+        tempMx.textContent = tempMax + "°C";
+        tempMn.textContent = tempMin + "°C";
+      }else if (weatherStatus === 'cloudy'){
+        wDate.textContent = today;
+        wStat.textContent = "cloudy";
+        wImg.src = "images/cloudy.svg";
+        bgWeather.src = "images/cloudy.jpg";
+        tempMx.textContent = tempMax + "°C";
+        tempMn.textContent = tempMin + "°C";
+      }else if (weatherStatus === 'fog'){
+        wDate.textContent = today;
+        wStat.textContent = "fog";
+        wImg.src = "images/fog.svg";
+        bgWeather.src = "images/fog.jpg";
+        tempMx.textContent = tempMax + "°C";
+        tempMn.textContent = tempMin + "°C";
+      }else if (weatherStatus === 'ishower'){
+        wDate.textContent = today;
+        wStat.textContent = "ishower";
+        wImg.src = "images/ishower.svg";
+        bgWeather.src = "images/ishower.jpg";
+        tempMx.textContent = tempMax + "°C";
+        tempMn.textContent = tempMin + "°C";
+      }else if (weatherStatus === 'oshower'){
+        wDate.textContent = today;
+        wStat.textContent = "ishower";
+        wImg.src = "images/ishower.svg";
+        bgWeather.src = "images/ishower.jpg";
+        tempMx.textContent = tempMax + "°C";
+        tempMn.textContent = tempMin + "°C";
+      }else if (weatherStatus === 'lightrain'){
+        wDate.textContent = today;
+        wStat.textContent = "lightrain";
+        wImg.src = "images/lightrain.svg";
+        bgWeather.src = "images/lightrain.jpg";
+        tempMx.textContent = tempMax + "°C";
+        tempMn.textContent = tempMin + "°C";
+      }else if (weatherStatus === 'lightsnow'){
+        wDate.textContent = today;
+        wStat.textContent = "lightsnow";
+        wImg.src = "images/lightsnow.svg";
+        bgWeather.src = "images/lightsnow.jpg";
+        tempMx.textContent = tempMax + "°C";
+        tempMn.textContent = tempMin + "°C";
+      }else if (weatherStatus === 'snow'){
+        wDate.textContent = today;
+        wStat.textContent = "snow";
+        wImg.src = "images/snow.svg";
+        bgWeather.src = "images/snow.jpg";
+        tempMx.textContent = tempMax + "°C";
+        tempMn.textContent = tempMin + "°C";
+      }else if (weatherStatus === 'mcloudy'){
+        wDate.textContent = today;
+        wStat.textContent = "mcloudy";
+        wImg.src = "images/cloudy.svg";
+        bgWeather.src = "images/cloudy.jpg";
+        tempMx.textContent = tempMax + "°C";
+        tempMn.textContent = tempMin + "°C";
+      }else if (weatherStatus === 'rain'){
+        wDate.textContent = today;
+        wStat.textContent = "rain";
+        wImg.src = "images/rain.svg";
+        bgWeather.src = "images/rain.jpg";
+        tempMx.textContent = tempMax + "°C";
+        tempMn.textContent = tempMin + "°C";
+      }else if (weatherStatus === 'rainsnow'){
+        wDate.textContent = today;
+        wStat.textContent = "rainsnow";
+        wImg.src = "images/rainsnow.svg";
+        bgWeather.src = "images/rainsnow.jpg";
+        tempMx.textContent = tempMax + "°C";
+        tempMn.textContent = tempMin + "°C";
+      }else if (weatherStatus === 'ts'){
+        wDate.textContent = today;
+        wStat.textContent = "tsrain";
+        wImg.src = "images/tsrain.svg";
+        bgWeather.src = "images/tsrain.jpg";
+        tempMx.textContent = tempMax + "°C";
+        tempMn.textContent = tempMin + "°C";
+      }else if (weatherStatus === 'ts'){
+        wDate.textContent = today;
+        wStat.textContent = "tstorm";
+        wImg.src = "images/tstorm.svg";
+        bgWeather.src = "images/tstorm.jpg";
+        tempMx.textContent = tempMax + "°C";
+        tempMn.textContent = tempMin + "°C";
+      }
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+    }
+  }
+
+fetchWeather(longitude,latitude);
+//Function for getting the weather forecast information from selected option
+selectCity.addEventListener('change',function(){
+    fetchStat.style.display = "block";
+     //Storing the clicked option in a varible
+     let cityValue = selectCity.value;
+     cityName.textContent = cityValue;
+     //Looping through the array citiesData to compare the value clicked (option) with the one stored in citiesData
+     for (let i=0; i < citiesData.length; i++){
+        let cityGroup = citiesData[i];
+        for (let j=0; j < cityGroup.length; j++){
+        //If the condition is true, we call the citiesLonlat function to use the 7timer API and retrieve the weather forecast for the longitude and latitude of the selected city
+            if(cityValue===cityGroup[j].name){
+                getWeatherData(cityGroup[j].lon, cityGroup[j].lat);
+                fetchWeather(cityGroup[j].lon, cityGroup[j].lat);
+                break;
+            }
+        }
+     }
+})
+
+//This function converts a date number (e.g., 20231115) into a string format separated by hyphens (e.g., 2023-11-15)
+function readableDate(dateNumber){
+    let d = dateNumber.toString();
+    let year = d.substring(0, 4);
+    let month = d.substring(4, 6);
+    let day = d.substring(6, 8);
+    let date = year + "-" + month + "-" + day;
+    return date;
+}
